893e83da187ac52573fff8827f8f96b7afb43bee
migle <longforfreedom+github@gmail.com>
2017-04-14 18:17:46 +0800

    linear

---
+++ linear_regression.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python
+# -*- coding:utf-8 -*-
+class LinearRegression:
+    def __init__(self):
+        pass
+    def train(self,x,y):
+        self.theta=x
+if __name__ =='__main__':
+    
