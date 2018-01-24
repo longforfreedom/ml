dc4b0bb5aca85d1f565f875a72e8aaccc8234fba
miyong <miyong@asiainfo.com>
2018-01-24 18:08:20 +0800

    x

--- data/captcha/gen_captcha.py
+++ data/captcha/gen_captcha.py
@@ -3,15 +3,15 @@
 import random
 import string
 import sys
-import math
 from PIL import Image,ImageDraw,ImageFont,ImageFilter
-from __future__ import print_function
+
+
 #字体的位置，不同版本的系统会有不同
 font_path = './fonts/simsun.ttc'
 #生成几位数的验证码
-number = 4
+number = 1
 #生成验证码图片的高度和宽度
-size = (100,30)
+size = (20,20)
 #背景颜色，默认为白色
 bgcolor = (255,255,255)
 #字体颜色，默认为蓝色
@@ -37,23 +37,44 @@ def gene_line(draw,width,height):
     draw.line([begin, end], fill = linecolor)
  
 #生成验证码
-def gene_captcha():
+def gene_captcha(filename,code):
     width,height = size #宽和高
     image = Image.new('RGB',(width,height),bgcolor) #创建图片
     font = ImageFont.truetype(font_path,25) #验证码的字体
     draw = ImageDraw.Draw(image)  #创建画笔
-    text = gene_text() #生成字符串
-    font_width, font_height = font.getsize(text)
-    draw.text(((width - font_width) / number, (height - font_height) / number),text,
+    #text = gene_text() #生成字符串
+    font_width, font_height = font.getsize(code)
+    draw.text(((width - font_width) / number, (height - font_height) / number),code,
             font= font,fill=fontcolor) #填充字符串
     if draw_line:
         gene_line(draw,width,height)
     # image = image.transform((width+30,height+10), Image.AFFINE, (1,-0.3,0,-0.1,1,0),Image.BILINEAR)  #创建扭曲
     image = image.transform((width+20,height+10), Image.AFFINE, (1,-0.3,0,-0.1,1,0),Image.BILINEAR)  #创建扭曲
     image = image.filter(ImageFilter.EDGE_ENHANCE_MORE) #滤镜，边界加强
-    image.save("./test/"+text+'.jpg') #保存验证码图片
+    image.save(filename) #保存验证码图片
+
 if __name__ == "__main__":
-    print("需要生成:%s 个图片" % sys.argv[1])
-    for i in range(1,int(sys.argv[1])+1):
-        print("生成第%d个" % i)
-        gene_captcha()
+    
+    train_num = 600
+    test_num = 100
+    print("需要生成:%d 个图片" % (test_num+train_num))
+
+    train_label = open("train_label.txt",'w')
+    test_label = open("test_label.txt",'w')
+
+    for i in range(1,train_num+1):
+        print("生成第%d个训练" % i)
+        code = gene_text()
+        fpath =  "train/" + str(i) +".jpg"
+        gene_captcha(fpath,code)
+        train_label.write("%s\t%s\n" % (fpath,code))
+
+    for i in range(1,test_num+1):
+        print("生成第%d个测试" % i)
+        code = gene_text()
+        fpath =  "test/" + str(i) +".jpg"
+        gene_captcha(fpath,code)
+        test_label.write("%s\t%s\n" % (fpath,code))
+
+    train_label.close()
+    test_label.close()
