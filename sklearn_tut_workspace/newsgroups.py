f1763421dbf98c182b9f72d66266da9129def095
miyong <miyong@asiainfo.com>
2018-01-26 18:01:48 +0800

    x

---
+++ sklearn_tut_workspace/newsgroups.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python
+# -*- coding:utf-8 -*-
+
+from sklearn.datasets import fetch_20newsgroups
+from sklearn.feature_extraction.text import CountVectorizer
+from sklearn.feature_extraction.text import TfidfTransformer
+from sklearn.naive_bayes import MultinomialNB
+
+categories = ['alt.atheism', 'soc.religion.christian',
+              'comp.graphics', 'sci.med']
+twenty_train = fetch_20newsgroups(data_home="./data",
+    subset='train', categories=categories, shuffle=True, random_state=42)
+#print(dir(twenty_train))
+#['DESCR', 'data', 'description', 'filenames', 'target', 'target_names']
+#print(twenty_train.data[0])
+#print(twenty_train.target[0])
+#print(twenty_train.target_names[0])
+
+count_vect = CountVectorizer()
+X_train_counts = count_vect.fit_transform(twenty_train.data)
+print(type(X_train_counts))
+print(X_train_counts.shape)
+print(len(twenty_train.data))
+print(count_vect.vocabulary_.get('algorithm'))
+print(count_vect.vocabulary_.get('mail'))
+print('-'*20)
+
+tfidf_transformer = TfidfTransformer()
+X_train_tfidf = tfidf_transformer.fit_transform(X_train_counts)
+
+#print(X_train_tfidf.shape)
+print(X_train_counts[1])
+#print('-'*20)
+#print(X_train_tfidf[1])
+#print(dir(count_vect))
+#clf = MultinomialNB().fit(X_train_tfidf, twenty_train.target)
